3956b63e858679c1e399dcae27026b96a977d11a
VicDario <26585356+VicDario@users.noreply.github.com>
2021-06-03 17:18:59 -0400

    Only prints if there a change

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ function sort() {
     for (let min = 0; min < cardsOrder.length; min++) {
         for (let index = min +1; index < cardsOrder.length; index++) {
             let aux = cardsOrder[min];
+            
             if (aux[0] == "J") aux[0] = 11;
             if (aux[0] == "Q") aux[0] = 12;
             if (aux[0] == "K") aux[0] = 13;
@@ -79,65 +80,68 @@ function sort() {
             if (aux2[0] == "Q") aux2[0] = 12;
             if (aux2[0] == "K") aux2[0] = 13;
             if (aux2[0] == "A") aux2[0] = 1;
-
+            let change = false;
             if (aux[0] > aux2[0]) {
+                change = true
+                console.log("algo cambio");
                 let aux3 = cardsOrder[min];
                 cardsOrder[min] = cardsOrder[index];
                 cardsOrder[index] = aux3;
             }
-
-            let cardBox = document.createElement("div");
-            cardBox.classList.add("box");
-            let number = document.createElement("p");
-            number.innerHTML = count;
-            cardBox.appendChild(number);
-            for (const value of cardsOrder) {
-                let cardValue = value[0];
-                let cardSuit = value[1];
-
-                if (cardValue == 11) cardValue = "J";
-                if (cardValue == 12) cardValue = "Q";
-                if (cardValue == 13) cardValue = "K";
-                if (cardValue == 1) cardValue = "A";
-
-                let valueToCard = document.createElement("P");
-                let suitToCard = document.createElement("P");
-                let suitToCard2 = document.createElement("P");
-
-                if (cardSuit == "♥" || cardSuit == "♦") {
-                    suitToCard.style.color = "red";
-                    suitToCard2.style.color = "red";
-                } else {
-                    suitToCard.style.color = "black";
-                    suitToCard2.style.color = "black";
+            if(change){
+                let cardBox = document.createElement("div");
+                cardBox.classList.add("box");
+                let number = document.createElement("p");
+                number.innerHTML = count;
+                cardBox.appendChild(number);
+                for (const value of cardsOrder) {
+                    let cardValue = value[0];
+                    let cardSuit = value[1];
+    
+                    if (cardValue == 11) cardValue = "J";
+                    if (cardValue == 12) cardValue = "Q";
+                    if (cardValue == 13) cardValue = "K";
+                    if (cardValue == 1) cardValue = "A";
+    
+                    let valueToCard = document.createElement("P");
+                    let suitToCard = document.createElement("P");
+                    let suitToCard2 = document.createElement("P");
+    
+                    if (cardSuit == "♥" || cardSuit == "♦") {
+                        suitToCard.style.color = "red";
+                        suitToCard2.style.color = "red";
+                    } else {
+                        suitToCard.style.color = "black";
+                        suitToCard2.style.color = "black";
+                    }
+    
+                    valueToCard.innerHTML = `${cardValue}`;
+                    suitToCard.innerHTML = `${cardSuit}`;
+                    suitToCard2.innerHTML = `${cardSuit}`;
+    
+                    let cardHead = document.createElement("div");
+                    let cardBody = document.createElement("div");
+                    let cardFoot = document.createElement("div");
+    
+                    cardHead.classList.add("head");
+                    cardBody.classList.add("body");
+                    cardFoot.classList.add("foot");
+    
+                    cardHead.appendChild(suitToCard);
+                    cardBody.appendChild(valueToCard);
+                    cardFoot.appendChild(suitToCard2);
+    
+                    let card = document.createElement("div");
+                    card.classList.add("card");
+                    card.appendChild(cardHead);
+                    card.appendChild(cardBody);
+                    card.appendChild(cardFoot);
+    
+                    cardBox.appendChild(card);
                 }
-
-                valueToCard.innerHTML = `${cardValue}`;
-                suitToCard.innerHTML = `${cardSuit}`;
-                suitToCard2.innerHTML = `${cardSuit}`;
-
-                let cardHead = document.createElement("div");
-                let cardBody = document.createElement("div");
-                let cardFoot = document.createElement("div");
-
-                cardHead.classList.add("head");
-                cardBody.classList.add("body");
-                cardFoot.classList.add("foot");
-
-                cardHead.appendChild(suitToCard);
-                cardBody.appendChild(valueToCard);
-                cardFoot.appendChild(suitToCard2);
-
-                let card = document.createElement("div");
-                card.classList.add("card");
-                card.appendChild(cardHead);
-                card.appendChild(cardBody);
-                card.appendChild(cardFoot);
-
-                cardBox.appendChild(card);
+                container.appendChild(cardBox);
+                count++;
             }
-            container.appendChild(cardBox);
-            count++;
         }
     }
 }
